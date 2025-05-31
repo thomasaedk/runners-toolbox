0b1e4f31fb56c7718c770578a9265f6d64a5cb00
Thomas Aegidiussen <thomasaedk@gmail.com>
2025-05-31 20:55:23 +0200

    Revert flask app port to 5000

--- backend/app.py
+++ backend/app.py
@@ -135,6 +135,5 @@ def test():
     return jsonify({'message': 'Backend is working!'})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
     debug = os.environ.get('FLASK_ENV') != 'production'
-    app.run(host='0.0.0.0', port=port, debug=debug)
+    app.run(host='0.0.0.0', port=5000, debug=debug)
