a6f8a59f5daf9ae652190973735e751a41c39556
Thomas Aegidiussen <thomasaedk@gmail.com>
2025-05-31 20:43:36 +0200

    Update flask app to get port from env variable. Default to 5000

--- backend/app.py
+++ backend/app.py
@@ -135,4 +135,6 @@ def test():
     return jsonify({'message': 'Backend is working!'})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    debug = os.environ.get('FLASK_ENV') != 'production'
+    app.run(host='0.0.0.0', port=port, debug=debug)
